8369ab2c0e133c7d9b55745cc344e0f9cb6c8b25
MagdalenaRa <magdalenaarams@gmail.com>
2024-12-13 11:01:53 +0100

    delete-transaction

--- controllers/transactions/deleteTransaction.js
+++ controllers/transactions/deleteTransaction.js
@@ -1,5 +1,21 @@
-const deleteTransaction = async (req, res, next) => {
-  res.json({ message: 'delete transaction' })
-}
+import Transaction from '../../models/transactionSchema.js'; 
+import { StatusCodes } from 'http-status-codes'; 
 
-export default deleteTransaction
+
+const deleteTransaction = async (req, res, next) => {
+  const { transactionId } = req.params; 
+
+  try {
+    const transaction = await Transaction.findByIdAndDelete(transactionId); 
+
+    if (!transaction) {
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Transaction not found' }); 
+    }
+
+    res.status(StatusCodes.OK).json({ message: 'Transaction deleted successfully' }); 
+  } catch (error) {
+    next(error); 
+  }
+};
+
+export default deleteTransaction;
