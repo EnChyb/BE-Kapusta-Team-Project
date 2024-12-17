b21642d7986457cd7f249f9841eff2abef0231d7
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-12-17 11:16:28 +0100

    authenticateToken middleware fixed

--- app.js
+++ app.js
@@ -10,22 +10,24 @@ import authRouter from "./routes/api/auth.js";
 import transactionRouter from "./routes/api/transaction.js";
 import userRouter from "./routes/api/user.js";
 
+import authenticateToken from './middleware/authenticateToken.js';
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(morgan(formatsLogger));
 
-// CORS configuration - only development and production of frontend can fetch data
-const corsOptions = {
-  development: {
-    origin: "http://localhost:3000"
-  },
-  production: {
-    origin: "https://fe-kapusta-team-project.vercel.app/"
-  }
-};
-const environment = process.env.NODE_ENV || "development"; // check NODE_ENV in scripts
+// // CORS configuration - only development and production of frontend can fetch data
+// const corsOptions = {
+//   development: {
+//     origin: "http://localhost:3000"
+//   },
+//   production: {
+//     origin: "https://fe-kapusta-team-project.vercel.app/"
+//   }
+// };
+// const environment = process.env.NODE_ENV || "development"; // check NODE_ENV in scripts
 
 // app.use(cors(corsOptions[environment]));
 app.use(cors());
