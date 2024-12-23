8e812a47239f063fa01c1ca7b630209bf399e5d0
MagdalenaRa <magdalenaarams@gmail.com>
2024-12-23 19:23:54 +0100

    swagger-update
    dokumentacja punktów końcowych (swagger-ui)

--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -18,12 +18,51 @@ router.post('/expense', authenticateToken, addExpense);
 router.post('/income', authenticateToken, addIncome);
 router.get('/income', authenticateToken, getIncome);
 router.get('/expense', authenticateToken, getExpense);
-// router.delete('/:transactionId', authenticateToken, deleteTransaction);
+router.delete('/:transactionId', authenticateToken, deleteTransaction);
 router.get('/income-categories', authenticateToken, getIncomesCat);
 router.get('/expense-categories', authenticateToken, getExpenseCat);
 router.get('/period-data', authenticateToken, getPeriodDataTransactions);
 router.get('/user-transactions', authenticateToken, getUserTransactions);
+router.post('/', authenticateToken, addTransaction);
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Transaction:
+ *       type: object
+ *       required:
+ *         - description
+ *         - category
+ *         - amount
+ *         - date
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the transaction
+ *         description:
+ *           type: string
+ *           description: Description of the transaction
+ *           example: "Zakup artykułów spożywczych"
+ *         category:
+ *           type: string
+ *           description: Category of the transaction
+ *           enum: [Products, Alcohol, Entertainment, Health, Transport, Housing, Technique, Communal, Communication, Sports, Hobbies, Education, Other, Salary, Bonus]
+ *           example: "Products"
+ *         amount:
+ *           type: number
+ *           description: Amount of the transaction
+ *           example: 100.5
+ *         date:
+ *           type: string
+ *           format: date
+ *           description: Date of the transaction
+ *           example: "2024-12-18"
+ *         owner:
+ *           type: string
+ *           description: ID of the user who made the transaction
+ *           example: "5f8f8c44b54764421b7168f5"
+ */
 
 /**
  * @swagger
@@ -36,56 +75,19 @@ router.get('/user-transactions', authenticateToken, getUserTransactions);
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             properties:
- *               amount:
- *                 type: number
- *               type:
- *                 type: string
- *                 enum: [income, expense]
- *               category:
- *                 type: string
- *               date:
- *                 type: string
- *                 format: date-time
- *               description:
- *                 type: string
- *               userId:
- *                 type: string
- *                 format: uuid
+ *             $ref: '#/components/schemas/Transaction'
  *     responses:
  *       201:
  *         description: Transakcja dodana pomyślnie
  *         content:
  *           application/json:
- *             type: object
- *             properties:
- *               message:
- *                 type: string
- *               code:
- *                 type: integer
- *               data:
- *                 type: object
- *                 properties:
- *                   amount:
- *                     type: number
- *                   type:
- *                     type: string
- *                     enum: [income, expense]
- *                   category:
- *                     type: string
- *                   date:
- *                     type: string
- *                     format: date-time
- *                   description:
- *                     type: string
- *                   userId:
- *                     type: string
- *                     format: uuid
+ *             schema:
+ *               $ref: '#/components/schemas/Transaction'
+ *       400:
+ *         description: Invalid input
  *       500:
  *         description: Błąd serwera
  */
-// router.post('/', authenticateToken, addTransaction);
 
 /**
  * @swagger
@@ -108,7 +110,8 @@ router.get('/user-transactions', authenticateToken, getUserTransactions);
  *       500:
  *         description: Błąd serwera
  */
-router.delete('/:transactionId', authenticateToken, deleteTransaction);
+
+
 
 
 export default router;
--- utils/swaggerConfig.js
+++ utils/swaggerConfig.js
@@ -1,4 +1,9 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import transactionsRouter from '../routes/api/transaction.js';
+
+const app = express();
 
 const options = {
   definition: {
@@ -13,6 +18,15 @@ const options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(express.json());
+app.use('/api/transactions', transactionsRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 
 export default swaggerSpec
 
