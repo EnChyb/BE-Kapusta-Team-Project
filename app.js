5a362c72a4dcf03413f65249c09d093fa32f6f3e
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-12-29 13:17:28 +0100

    CORS

--- app.js
+++ app.js
@@ -34,11 +34,14 @@ const corsOptions = {
   development: {
     origin: ["http://localhost:5173", "http://localhost:3000"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   },
   production: {
     origin: "https://fe-kapusta-team-project.vercel.app",
-    credentials: true
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }
 };
 
